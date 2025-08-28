1c4706c54b8e1933344b4653077f0d2251b35240
Farhan Sadik <farhansadik0760@gmail.com>
2025-08-28 08:34:39 +0600

    cors fix

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
-const express = require("express")
-const songRoutes = require("./routes/song.route")
+const express = require("express");
+const songRoutes = require("./routes/song.route");
+const cors = require("cors");
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.use("/", songRoutes)
+app.use("/", songRoutes);
 
-module.exports = app
+module.exports = app;
